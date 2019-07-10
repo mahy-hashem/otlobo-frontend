0aecf448ed82ae9e26ea67532465e9825bc1d9e9
Baheya Khalifa <beya@Baheyas-MacBook-Pro.local>
2019-07-10 15:42:01 +0200

    added fetchRestaurants functionality

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,18 +1,62 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import axios from "axios";
 
-import RestaurantDetailsPage from "./components/RestaurantDetailsPage";
+import RestaurantCard from "./RestaurantCard";
+import RestaurantDetailsPage from "./RestaurantDetailsPage";
 
-class Restaurant extends React.Component {
+class Restaurants extends React.Component {
+  state = {
+    restaurants: []
+  };
+
+  componentDidMount() {
+    this.fetchRestaurants();
+  }
+
+  fetchRestaurants = () => {
+    axios
+      .get("http://localhost:8080/restaurants")
+      .then(res => {
+        this.setState(
+          {
+            restaurants: res.data.restaurants
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
       <div>
-        <Route
-          path="/restaurant/:restaurantId"
-          component={RestaurantDetailsPage}
-        />
+        <Switch>
+          <Route path="/restaurants">
+            {this.state.restaurants.map(restaurant => {
+              const { id, name, address } = restaurant;
+              return (
+                <RestaurantCard
+                  key={id}
+                  id={id}
+                  name={name}
+                  address={address}
+                />
+              );
+            })}
+          </Route>
+          <Route
+            path="/restaurant/:restaurantId"
+            component={RestaurantDetailsPage}
+          />
+        </Switch>
       </div>
     );
   }
 }
 
-export default Restaurant;
+export default Restaurants;
