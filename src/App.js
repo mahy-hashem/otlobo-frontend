f6c8f0e85c0200dcbc3201e21ef86edd0e55c4f4
Baheya Khalifa <beya@Baheyas-MacBook-Pro.local>
2019-07-10 14:34:26 +0200

    added Restaurants path

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React from "react";
 import { Route, Switch } from "react-router-dom";
-import SignUp from "./components/SignUp"
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import SignUp from "./components/SignUp";
+import Restaurants from "./components/Restaurant";
 
 class App extends React.Component {
   render() {
@@ -10,9 +12,10 @@ class App extends React.Component {
       <div>
         <Switch>
           <Route path="/signUp" component={SignUp} />
+          <Route path="/restaurants" component={Restaurants} />
         </Switch>
       </div>
-    )
+    );
   }
 }
 
