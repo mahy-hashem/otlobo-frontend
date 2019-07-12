15ceed8ba2f5a6a8e6675b55f9d88b68f71697bd
Baheya Khalifa <beya@Baheyas-MacBook-Pro.local>
2019-07-12 17:25:54 +0200

    made breadcrumb more generic and reusable

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -5,12 +5,7 @@ import "./Breadcrumb.scss";
 
 class Breadcrumb extends React.Component {
   render() {
-    return (
-      <BreadCrumb>
-        <BreadCrumb.Item href="#">Home</BreadCrumb.Item>
-        <BreadCrumb.Item href="#">Restaurants</BreadCrumb.Item>
-      </BreadCrumb>
-    );
+    return <BreadCrumb>{this.props.children}</BreadCrumb>;
   }
 }
 
