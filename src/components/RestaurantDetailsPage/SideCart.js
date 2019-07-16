d090a044a76edd1248f2cbfca5bf8e928d078cc2
Baheya Khalifa <beya@Baheyas-MacBook-Pro.local>
2019-07-16 13:58:09 +0200

    added sidecart component

--- src/components/RestaurantDetailsPage/SideCart.js
+++ src/components/RestaurantDetailsPage/SideCart.js
@@ -1,8 +1,62 @@
 import React from "react";
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import GenericButton from "../GenericButton/GenericButton";
+
 class SideCart extends React.Component {
   render() {
-    return <div />;
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <h4>Your Cart</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h5>Restaurant Name</h5>
+          </Col>
+        </Row>
+        {this.props.order.map(item => {
+          const { id, name, price } = item;
+          return (
+            <Row>
+              <Col>
+                <p>Quantity</p>
+              </Col>
+              <Col>
+                <p>{name}</p>
+              </Col>
+              <Col>
+                <p>{price}</p>
+              </Col>
+            </Row>
+          );
+        })}
+        <Row>
+          <Col>
+            <p>Subtotal: Price</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p>Delivery fee: Price</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p>Total price: Price</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <GenericButton content="Proceed to Checkout" />
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
 
