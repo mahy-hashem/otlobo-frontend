292c9c507761e5654eda8b117b2d7de3af169bdf
fatmaMGomaa <fatma-mohamed@feps.edu.eg>
2019-07-17 05:35:59 +0200

    adding styling and make it responsive

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Header from "./components/Header/Header";
 import SignUp from "./components/Signup/SignUp";
 import Login from "./components/Login/Login";
-import LandingPage from "./components/LandingPage/LnadingPage";
+import LandingPage from "./components/LandingPage/LandingPage";
 import Restaurants from "./components/Restaurants/Restaurants";
 import RestaurantDetailsPage from "./components/RestaurantDetailsPage/RestaurantDetailsPage";
 import "./App.css";
@@ -11,8 +11,8 @@ import "./App.css";
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Switch>
+      <div className="container">
+        <Switch className="container">
           <Route exact path="/" component={LandingPage} />
           <Route path="/signUp" component={SignUp} />
           <Route path="/login" component={Login} />
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+import "./index.scss";
 
 ReactDOM.render(
   <BrowserRouter>
